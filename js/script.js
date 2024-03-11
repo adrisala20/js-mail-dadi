078131321f7a879cfe13a9a53e7567185605fef2
adrisala20 <adriana_15_tuya@hotmail.com>
2024-03-11 15:03:40 +0100

    add html structure

---
+++ js/script.js
@@ -0,0 +1,51 @@
+/* Mail
+Chiedi all’utente la sua email,
+controlla che sia nella lista di chi può accedere,
+stampa un messaggio appropriato sull’esito del controllo.
+
+*/
+
+// salvo la variabile in un bottone 
+const bottone = document.getElementById ('bottone');
+console.log('bottone');
+
+//creo l'evento per il bottone con il click
+bottone.addEventListener('click', function (){
+
+    //creo la variabile
+    let userMail = document.getElementById('mail').value;
+
+    let userInvitati = [
+        "andra.salas@gmail.com",
+        "vittoria.neroni@gmail.com",
+        "enrico.capari@gmail.com",
+        "juliana.ottoni@gmail.com"
+    ]
+    //inizio il ciclo
+    
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* Gioco dei dadi
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto.
+Prima di partire a scrivere codice poniamoci qualche domanda:
+Che ci sia un array da qualche parte?
+Se dobbiamo confrontare qualcosa che "cosa" ci serve?*/
